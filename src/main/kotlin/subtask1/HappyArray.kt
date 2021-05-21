d7883bb0a8d998dfde30ea84dc441ad6de0c3b54
Dietmar <tcodmos@gmail.com>
2021-05-21 18:33:51 +0300

    feat: solution tasks 1..3

--- src/main/kotlin/subtask1/HappyArray.kt
+++ src/main/kotlin/subtask1/HappyArray.kt
@@ -1,9 +1,28 @@
 package subtask1
 
 class HappyArray {
-
-    // TODO: Complete the following function
     fun convertToHappy(sadArray: IntArray): IntArray {
-        throw NotImplementedError("Not implemented")
+        var array = sadArray
+
+        while (!isHappyArray(array)){
+            val happyElements = arrayListOf(array[0])
+
+            for (i in 1..(array.size - 2))
+                if (array[i - 1] + array[i + 1] > array[i])
+                    happyElements.add(array[i])
+
+            happyElements.add(array[array.size - 1])
+
+            array = happyElements.toIntArray()
+        }
+
+        return array
+    }
+
+    private fun isHappyArray(array: IntArray): Boolean {
+        for (i in 1..(array.size - 2))
+            if (array[i - 1] + array[i + 1] < array[i])
+                return false
+        return true
     }
 }
--- src/main/kotlin/subtask2/BillCounter.kt
+++ src/main/kotlin/subtask2/BillCounter.kt
@@ -1,10 +1,8 @@
 package subtask2
 
 class BillCounter {
-
-    // TODO: Complete the following function
-    // The output could be "Bon Appetit" or the string with number(e.g "10")
     fun calculateFairlySplit(bill: IntArray, k: Int, b: Int): String {
-        throw NotImplementedError("Not implemented")
+        val sumWithoutK = (bill.sum() - bill[k]) / 2
+        return if (b <= sumWithoutK) "bon appetit" else "${b - sumWithoutK}"
     }
 }
